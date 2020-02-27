8e83332fb44d9221a8ee5dd7b09c87bc7adc2a8b
Muh. Shofuwan Anwar <44674870+Shofuuu@users.noreply.github.com>
2020-02-27 15:12:43 +0700

    Create job4_27_Feb_2020.py

---
+++ job4_27_Feb_2020.py
@@ -0,0 +1,14 @@
+print("Tebak Segitiga")
+sisi_a = int(input("Masukkan Sisi A : "))
+sisi_b = int(input("Masukkan Sisi B : "))
+sisi_c = int(input("Masukkan Sisi C : "))
+flag = False
+
+if sisi_a <= (sisi_b + sisi_c):
+    flag = True
+if sisi_b <= (sisi_c + sisi_a):
+    flag = True
+if sisi_c <= (sisi_a + sisi_b):
+    flag = True
+
+print("Hasil Cek : ", "YA" if flag == True else  "TIDAK")
